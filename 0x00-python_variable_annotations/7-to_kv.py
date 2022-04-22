468b594030a60b28ebe9b3fbc8b50e365755d789
nuel07 <yehanganep@gmail.com>
2022-04-22 05:33:21 -0700

    task 7

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+''' type annotated function that returns a tuple '''
+from typing import Tuple, Union
+
+
+def to_kv(k: str, v: Union[int, float]) ->Tuple[str, float]:
+    ''' returns a tuple of a string and float
+    Args:
+        k(str): the string
+        v(int or float): the number
+    return: tuple
+    '''
+    return (k, v * v)
