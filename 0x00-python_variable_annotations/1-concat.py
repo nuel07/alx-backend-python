c3dd3a80abcf46d98405a634220cbf6d851cc38f
nuel07 <yehanganep@gmail.com>
2022-04-22 03:41:23 -0700

    task 1

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,12 @@
+#1/usr/bin/env python3
+''' A type annotated function '''
+
+
+def concat(str1: str, str2: str) -> str:
+    ''' concatenates two strings
+    Args:
+        str1(string): the first string
+        str2(string): the second string
+    return: string
+    '''
+    return str1 + str2
