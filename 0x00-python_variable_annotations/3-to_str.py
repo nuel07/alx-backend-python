e2252fc6d0294d77c9202f3d1b25a44d4b53c7d0
nuel07 <yehanganep@gmail.com>
2022-04-22 04:13:45 -0700

    task 3

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+''' type annotated function that stringifies a float'''
+
+
+def to_str(n: float) -> str:
+    ''' returns string representation of the float
+    Args:
+        n(float): the float value
+    return: str
+    '''
+    return str(n)
