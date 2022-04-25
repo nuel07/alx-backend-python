da26122960f040ef5ee3a0128e9c1ad6ce5c06ae
nuel07 <yehanganep@gmail.com>
2022-04-25 05:48:32 -0700

    annotation fix

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -2,9 +2,9 @@
 ''' An async generator '''
 import random
 import asyncio
+from typing import Generator
 
-
-async def async_generator() -> float:
+async def async_generator() -> Generator[float, None, None]:
     ''' yields a random number between 0 and 10 '''
     for x in range(10):
         await asyncio.sleep(1)
