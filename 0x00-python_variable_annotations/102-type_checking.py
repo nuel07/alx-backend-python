bf3a76801c62146f8d1dbe2b0552b65fb4a4a6c9
nuel07 <yehanganep@gmail.com>
2022-06-04 03:02:24 -0700

    task 102

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -1,18 +1,16 @@
 #!/usr/bin/env python3
 ''' code to be checked with mypy '''
-from typing import Tuple, Any, List, Union
+from typing import Tuple, Any, List
 
 
-def zoom_array(lst: List, factor: int = 2) -> Tuple:
-    zoomed_in: Union[Tuple, List] = [
-        item for item in lst
-        for i in range(factor)
-    ]
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
+    """return zoomed_in"""
+    zoomed_in: List = [        
+        item for item in lst        
+        for i in range(factor)        
+    ]    
     return zoomed_in
 
-
 array = [12, 72, 91]
-
-zoom_2x = zoom_array(array)
-
-zoom_3x = zoom_array(array, 3)
+zoom_2x = zoom_array(tuple(array))
+zoom_3x = zoom_array(tuple(array), int(3.0)
